a0d977c9ed59a1de54a80e1b75e5d974f0ca3367
cauasilva2 <116925139+cauasilva2@users.noreply.github.com>
2024-05-02 20:00:58 -0300

    kkkk

--- main.js
+++ main.js
@@ -15,8 +15,14 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll('.contador');
 const tempoObjetivo1 = new Date('2024-10-05T00:00:00');
+const tempoObjetivo2 = new Date('2024-10-05T00:00:00');
+const tempoObjetivo3 = new Date('2024-10-05T00:00:00');
+const tempoObjetivo4 = new Date('2024-10-05T00:00:00');
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempoObjetivo1 = new Date('2024-10-05T00:00:00');
+const tempoObjetivo1 = new Date('2024-10-05T00:00:00');
+const tempoObjetivo1 = new Date('2024-10-05T00:00:00');
+const tempoObjetivo1 = new Date('2024-10-05T00:00:00');
 
 function calculaTempo(tempoObjetivo1) {
     let tempoAtual = new Date();
