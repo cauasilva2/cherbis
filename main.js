9d195e9af9dca0467850cfb8248f808e0e0b6697
cauasilva2 <116925139+cauasilva2@users.noreply.github.com>
2024-05-09 19:59:25 -0300

    erfdfdf

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function calculaTempo(tempoObjetivo1) {
     horas %= 24;
     
     if (tempoFinal > 0) {
-    return dias + 'dias' + horas + 'horas' + minutos + 'minutos' + segundos +'segundos';
+    return dias + 'dias ' + horas + 'horas ' + minutos + 'minutos ' + segundos +'segundos ';
 }
 else {
     return 'Tempo Esgotado';
