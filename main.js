f9125a898d865f968f1e73fccacf92da2665a777
cauasilva2 <116925139+cauasilva2@users.noreply.github.com>
2024-05-09 19:57:15 -0300

    yhvgucu

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll('.contador');
-const tempoObjetivo1 = new Date('2023-05-05T00:00:00');
+const tempoObjetivo1 = new Date('2024-05-12T00:00:00');
 const tempoObjetivo2 = new Date('2024-06-25T00:00:00');
 const tempoObjetivo3 = new Date('2024-10-05T00:00:00');
 const tempoObjetivo4 = new Date('2024-12-10T00:00:00');
